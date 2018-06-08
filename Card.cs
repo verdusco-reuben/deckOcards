b190a21671eb203e5bbc410ce622182c87fc0846
Reuben <verdusco.reuben@gmail.com>
2018-06-08 15:40:49 -0700

    finished the demogit add .

---
+++ Card.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+
+namespace demo2
+{
+    class Card
+    {
+        public string stringVal;
+        public string suit;
+
+        public int val;
+
+        public Card(string strVal, string st, int v)
+        {
+            stringVal = strVal;
+            suit = st;
+            val = v;
+        }
+    }
+
+    class Deck
+    {
+        public List<Card> cards  = new List<Card>();
+        public Deck()
+        {
+            Reset();
+        }
+       public Card Deal()
+       {
+           Card tempCard = cards[0];
+           cards.RemoveAt(0);
+           return tempCard;
+       }
+
+       public void Reset()
+       {
+           List<Card> fakedeck = new List<Card>();
+            string[] demosuits =  {"Clubs", "Spades", "Hearts", "Diamonds"};
+            string[] demostringVals = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+            
+            for(int x = 0 ; x < demosuits.Length; x++)
+            {
+                for(int i = 0; i< demostringVals.Length; i++)
+                {
+                    Card newcard = new Card(demostringVals[i], demosuits[x], i);
+                    fakedeck.Add(newcard);
+                    // System.Console.WriteLine("Suit: "+newcard.suit+", Val: "+newcard.val);  
+                }             
+            }
+            cards = fakedeck;
+       }
+
+        public void Shuffle()
+        {
+            Random _random = new Random();
+            int n = cards.Count;
+            for (int i = 0; i < n; i++)
+            {
+                int rand = i + _random.Next(n - i);
+                Card temp = cards[rand];
+                cards[rand] = cards[i];
+                cards[i] = temp;
+                System.Console.WriteLine(cards[i].suit);
+            }
+        }
+
+    }
+    class Player
+    {
+        public string name;
+
+        public List<Card> hand;
+
+        public Player(string n)
+        {
+            name = n;
+            hand = new List<Card>();
+        }
+        public Card Draw(Deck curr_deck)
+        {
+            Card mycard = curr_deck.Deal();
+            hand.Add(mycard);
+            return mycard;
+        }
+        public Card Discard(int index)
+        {
+            if(index > hand.Count)
+            {
+                return new Card("null", "null", 0);
+            }
+            Card bad_card = hand[index];
+            hand.RemoveAt(index);
+            return bad_card;
+        }
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace demo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Deck testDeck = new Deck();
+            // System.Console.WriteLine(testDeck.cards.Count);
+            Player chris = new Player("Chris");
+            System.Console.WriteLine(chris.hand.Count);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            chris.Draw(testDeck);
+            
+            System.Console.WriteLine(chris.hand.Count);
+            
+            Card idk = chris.Discard(100);
+            System.Console.WriteLine(idk.stringVal);
+        }
+    }
+}
